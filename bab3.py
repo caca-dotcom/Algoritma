1c567e41b6781b00661e8acd3e6dbf331f16aa6c
caca-dotcom <rplcacaa@gmail.com>
2026-08-12 08:38:24 +0700

    Add files via upload

---
+++ bab3.py
@@ -0,0 +1,17 @@
+nama  = "Caca"
+print("nama  : ", nama)
+umur  = 16
+print("umur  : ", umur)
+tinggi = 158,5
+print("tinggi :", tinggi)
+Lulus = False
+print("Lulus :", Lulus) 
+
+nama produk = " sepatu lari"
+print("nama produk : ", nama produk)
+stok tersedia = 15
+print("stok tersedia :", stok tersedia)
+harga ($): 249.5
+print("harga :", harga)
+tersedia di toko: True
+print("tersedia di toko :", tersedia di toko)
